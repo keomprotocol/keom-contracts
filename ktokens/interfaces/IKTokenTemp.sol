dc036a74271d8877401bda3ed8fd9b21a7835471
daedlock <null>
2024-02-25 18:50:16 +0000

    remove old and temp contracts

--- ktokens/interfaces/IKTokenTemp.sol
+++
@@ -1,7 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity 0.8.4;
-
-interface IKTokenTemp {
-    /*** temp Functions ***/
-    function _burnOTokensFromKTokens(address[] calldata accounts, uint256 totalBurned) external;
-}
